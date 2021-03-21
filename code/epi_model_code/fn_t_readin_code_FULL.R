6699820c1098db7095914c58fcdc514b8f91f3af
Abigail L. Horn <abigaillhorn@gmail.com>
2021-03-20 22:56:46 -0700

    reduce Beta_t

--- code/epi_model_code/fn_t_readin_code_FULL.R
+++ code/epi_model_code/fn_t_readin_code_FULL.R
@@ -20,7 +20,7 @@ assign("mu.2", R0_redux2)
 assign("mu.3", R0_redux3) #0.31)
 assign("mu.3", R0_redux3) #0.31)
 assign("mu.4", 0.75*R0_redux3)
-assign("mu.5", 0.6*R0_redux3)
+assign("mu.5", 0.57*R0_redux3)
 
 mu_y <- as.vector(length(Beta_t))
 for (i in 1:length(Beta_t)){
